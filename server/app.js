664b4c47ac21c0314307632d9260472fe90f455f
arunajain <aruna.jaindev@gmail.com>
2025-07-17 09:18:33 -0600

    Working on Porject Directory Structure

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors'); 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dotenv = require('dotenv');
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,3 +20,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+
+
